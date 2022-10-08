9287f0a094522764676836416146641b4ae3d634
Александр Бабенко <Shapacakbalanar@gmail.com>
2022-10-08 20:06:57 +0700

    Рефакторинг функции представления views.home_page, теперь используется шаблон, так же изменен модульный тест, используется проверка на использование шаблона

--- superlists/list/tests.py
+++ superlists/list/tests.py
@@ -7,17 +7,7 @@ from list.views import home_page
 class HomePageTest(TestCase):
     '''Тестируем домашнюю страницу'''
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''тест: url-/ преобразуется в представление домашней станицы'''
-
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         '''тест: домашняя страница возвращает правильный html'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do list</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
