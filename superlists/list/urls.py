9013f665108de962364ed369a8bd43cb5e98be96
Александр Бабенко <Shapacakbalanar@gmail.com>
2022-10-26 23:53:39 +0700

    Git перенесен внутрь проекта django

--- superlists/list/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('new', views.list_new),
-    path('<int:id>/', views.view_list),
-    path('<int:id>/add_item', views.add_item),
-]
