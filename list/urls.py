5ea81a7c7834e98d1d2c0e96f67a40d465f68635
Александр Бабенко <Shapacakbalanar@gmail.com>
2022-11-19 19:52:56 +0700

    Рефакторизованы жестко закодированные URL в шаблонах

--- list/urls.py
+++ list/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new', views.list_new),
-    path('<int:id>/', views.view_list),
+    path('new', views.list_new, name='new_list'),
+    path('<int:id>/', views.view_list, name='view_list'),
 ]
