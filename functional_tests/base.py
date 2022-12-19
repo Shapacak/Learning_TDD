f665155e20a55fc778f4566a5a42886029c31e6e
Александр Бабенко <Shapacakbalanar@gmail.com>
2022-12-19 20:27:58 +0700

    В ФТ на вход в систему добавлен вспомогательнаый метод для проверки входящей почты через POP3

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,6 +10,18 @@ import time
 
 MAX_WAIT = 10
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 
 class FunctionalTest(StaticLiveServerTestCase):
     '''Функциональный тест'''
@@ -17,39 +29,22 @@ class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Chrome('./chromedriver/chromedriver.exe')
         #set_staging_server()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
 
     def tearDown(self) -> None:
         self.browser.quit()
 
+    @wait
     def wait_for(self, fn):
-        '''ожидание'''
-        start_time = time.time()
+        return fn()
 
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+    @wait
     def wait_for_row_in_list_table(self, text_row):
-        '''проверка на наличие текста в строках таблицы'''
-        start_time = time.time()
-
-        while True:
-            try:
-                table = self.browser.find_element(by=By.ID, value='id_list_table')
-                rows = table.find_elements(by=By.TAG_NAME, value='tr')
-                self.assertIn(text_row, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertIn(text_row, [row.text for row in rows])
 
     def input_box(self, text):
         '''вводим текст в поле ввода'''
@@ -58,15 +53,17 @@ class FunctionalTest(StaticLiveServerTestCase):
         inputbox.send_keys(text)
         inputbox.send_keys(Keys.ENTER)
 
+    @wait
     def wait_to_be_logged_in(self, email):
         '''ожидание входа в систему'''
-        self.wait_for(lambda: self.browser.find_element(by=By.LINK_TEXT, value='Log out'))
+        self.browser.find_element(by=By.LINK_TEXT, value='Log out')
         navbar = self.browser.find_element(by=By.CSS_SELECTOR, value='.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         '''ожидание выхода из системы'''
-        self.wait_for(lambda: self.browser.find_element(by=By.NAME, value='email'))
+        self.browser.find_element(by=By.NAME, value='email')
         navbar = self.browser.find_element(by=By.CSS_SELECTOR, value='.navbar')
         self.assertNotIn(email, navbar.text)
 
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -2,18 +2,56 @@ from django.core import mail
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import re
+import os
+import poplib
+import time
 from .base import FunctionalTest
+from config.config import set_gmail_password
 
-
-TEST_EMAIL = 'example@mail.com'
 SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
     '''тест регистрации в системе'''
 
+    def wait_for_email(self, test_email, subject):
+        '''ожидание эллектронного сообщения через POP3'''
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.gmail.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['GMAIL_PASSWORD'])
+            while time.time() - start < 60:
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count-10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf-8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                    else:
+                        time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
     def test_can_get_email_link_to_login(self):
         '''тест: можно получить ссылку дла регистрации по почте'''
+        if self.staging_server:
+            TEST_EMAIL = 'Shapacakbalanar@gmail.com'
+        else:
+            TEST_EMAIL = 'example@mail.com'
         # Я захожу на наш сайт суперсписков и замечаю новый элемент "Войти"
         # Он предлагает мне ввести свой email для этого
         self.browser.get(self.live_server_url)
@@ -25,16 +63,13 @@ class LoginTest(FunctionalTest):
                                              self.browser.find_element(by=By.TAG_NAME, value='body').text))
 
         # Я проверяю свою почту и нахожу там письмо
-        email = mail.outbox[0]
-        self.assertIn(TEST_EMAIL, email.to)
-        self.assertEqual(email.subject, SUBJECT)
+        body = self.wait_for_email(TEST_EMAIL, SUBJECT)
 
         # Оно содержит ссылку на url адресс
-        self.assertIn('Проверьте свою почту, мы отправили Вам ссылку для входа на сайт',
-                      self.browser.find_element(by=By.TAG_NAME, value='body').text)
-        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIn('Проверьте свою почту, мы отправили Вам ссылку для входа на сайт',body)
+        url_search = re.search(r'http://.+/.+$', body)
         if not url_search:
-            print(f'not found url in {email.body}')
+            self.fail(f'not found url in {body}')
         url = url_search.group(0)
         self.assertIn(self.live_server_url, url)
 
